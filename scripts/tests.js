a9b44897ff29c98be88db19c728b925ce235f422
hsiehdjames <hsiehdjames@csu.fullerton.edu>
2017-03-08 19:15:00 -0800

    Add files via upload

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,4 +1,6 @@
-QUnit.test("test", function(assert) {
+var QUnit;
+QUnit.test('test', function(assert) {
+    var App = window.App || {};
     var ds = new App.DataStore();
     //The problem with creating unit test for mytruck is initializing it
     //it needs an id and db
@@ -6,10 +8,10 @@ QUnit.test("test", function(assert) {
     assert.equal(ds.add('m@bond.com', 'tea'), undefined);
     assert.equal(ds.add('m@bond.com', 'tea'), undefined);
     assert.equal(ds.add('james@bond.com', 'eshpressho'), undefined);
-    assert.ok(ds.getAll(), "Passed!");
+    assert.ok(ds.getAll(), 'Passed!');
     assert.equal(ds.remove('james@bond.com'), undefined);
-    assert.ok(ds.getAll(), "Passed!");
-    assert.equal(ds.get('m@bond.com'), "tea");
+    assert.ok(ds.getAll(), 'Passed!');
+    assert.equal(ds.get('m@bond.com'), 'tea');
     assert.equal(ds.get('james@bond.com'), undefined);
     assert.equal(myTruck.createOrder({
         emailAddress: 'me@goldfinger.com',
